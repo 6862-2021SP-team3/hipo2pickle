54a1a77530a655b334f070f5a42d7b8c64502954
Sangbaek <sangbaek@mit.edu>
2021-06-10 17:46:27 -0400

    add energies of gen particles in df

--- root2pickle/root2pickleEPG.py
+++ root2pickle/root2pickleEPG.py
@@ -164,19 +164,19 @@ class root2pickle():
 
         # #spherical coordinates
         # eleGen = [df_electronGen["GenEpx"], df_electronGen["GenEpy"], df_electronGen["GenEpz"]]
-        # df_electronGen.loc[:, 'GenEe'] = getEnergy(eleGen, me)
+        df_electronGen.loc[:, 'GenEe'] = getEnergy(eleGen, me)
         # df_electronGen.loc[:, 'GenEp'] = mag(eleGen)
         # df_electronGen.loc[:, 'GenEtheta'] = getTheta(eleGen)
         # df_electronGen.loc[:, 'GenEphi'] = getPhi(eleGen)
 
         # proGen = [df_protonGen["GenPpx"], df_protonGen["GenPpy"], df_protonGen["GenPpz"]]
-        # df_protonGen.loc[:, 'GenPe'] = getEnergy(proGen, M)
+        df_protonGen.loc[:, 'GenPe'] = getEnergy(proGen, M)
         # df_protonGen.loc[:, 'GenPp'] = mag(proGen)
         # df_protonGen.loc[:, 'GenPtheta'] = getTheta(proGen)
         # df_protonGen.loc[:, 'GenPphi'] = getPhi(proGen)
 
         # gamGen = [df_gammaGen["GenGpx"], df_gammaGen["GenGpy"], df_gammaGen["GenGpz"]]
-        # # df_gammaGen.loc[:, 'GenGp'] = mag(gamGen)
+        df_gammaGen.loc[:, 'GenGp'] = mag(gamGen)
         # df_gammaGen.loc[:, 'GenGtheta'] = getTheta(gamGen)
         # df_gammaGen.loc[:, 'GenGphi'] = getPhi(gamGen)
 
